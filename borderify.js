b0af5b0e090d4dec1efd4968d69f0ef3257207b9
gabei <gabe1792@gmail.com>
2025-08-08 15:51:25 -0500

    Update manifest json

--- borderify.js
+++ borderify.js
@@ -1,1 +1,14 @@
 document.body.style.border = "5px solid red";
+
+
+
+
+
+
+
+
+async function getListOfLibraryNames(){
+    // This function will return a list of library names from the holdings list
+    let names = document.querySelectorAll('ul[data-testid="holding-list-details"] li strong');
+    return Array.from(names).map((name) => name.innerText);
+}
