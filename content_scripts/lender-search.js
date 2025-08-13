c1d52e44293748fdbbdd0b7c11a6e0bd8e332fe5
gabei <gabe1792@gmail.com>
2025-08-13 18:29:23 -0500

    stripping extension down to very basics

--- content_scripts/lender-search.js
+++ content_scripts/lender-search.js
@@ -1,36 +1,13 @@
-(() => {
-  /**
-   * Check and set a global guard variable.
-   * If this content script is injected into the same page again,
-   * it will do nothing next time.
-   */
-  if (window.hasRun) {
-    return;
-  }
-  window.hasRun = true;
-  
-  async function getListOfLibraryNames(){
+async function getListOfLibraryNames(){
     // This function will return a list of library names from the holdings list
     let names = document.querySelectorAll('li[data-testid*="library-card"]');
-    console.log(names)
+    console.log("Library Names: ", names)
     return Array.from(names).map((name) => name.innerText);
-}
-  function clearList(){
 
-  }
+async function handleMessage(request, sender, sendResponse){
+  console.log(`A content script sent a message: ${request.greeting}`);
+  let list = await getListOfLibraryNames();
+  sendResponse({ response: list });
+};
 
-  /**
-   * Listen for messages from the background script.
-   * Call "insertBeast()" or "removeExistingBeasts()".
-   */
-  browser.runtime.onMessage.addListener(async (message) => {
-    if (message.command === "beastify") {
-      let list = await getListOfLibraryNames();
-      browser.runtime.sendMessage({
-        data: list
-      })
-    } else if (message.command === "reset") {
-      clearList();
-    }
-  });
-})();
+browser.runtime.onMessage.addListener(handleMessage);
--- popup/ui.js
+++ popup/ui.js
@@ -1,49 +1,46 @@
+// working from here: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_second_WebExtension
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/runtime/sendMessage
 
 const searchButton = document.querySelector("#search-button")
 const searchResults = document.querySelector("#search-results__list")
 const lenderList = [];
 
 searchButton.addEventListener("click", async ()=> {
-    console.log(document.title);
-    
+    notifyBackgroundPage();
 })
 
-function reset(){
-    browser.tabs.sendMessage(tabs[0].id, {
-          command: "reset",
-        });
+function handleResponse(message) {
+  console.log(`Message from the background script: ${message.response}`);
 }
 
-function search(){
-    browser.tabs.sendMessage(tabs[0].id, {command: "search"})
+function handleError(error) {
+  console.log(`Error: ${error}`);
 }
 
-function main(){
-    if (e.target.type === "search") {
-        browser.tabs
-        .query({ active: true, currentWindow: true })
-        .then(search)
-        .catch(reportError);
-    } else if (e.target.type === "reset") {
-        browser.tabs
-        .query({ active: true, currentWindow: true })
-        .then(reset)
-        .catch(reportError);
-    }
-
-    browser.tabs
-        .executeScript({ file: "/content_scripts/lender-search.js" })
-        .then(main)
-        .then((data) => {
-            console.log(data);
-            data.forEach((item) => {
-            let li = new HTMLElement("li");
-            li.textContent = item;
-            searchResults.append(li);
-        })
-    })
-    .catch(reportExecuteScriptError);
+function notifyBackgroundPage(e) {
+  const sending = browser.sendMessage({
+    command: "search",
+  });
+  sending.then(handleResponse, handleError);
 }
 
 
 
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+  document.querySelector("#popup-content").classList.add("hidden");
+  document.querySelector("#error-content").classList.remove("hidden");
+  console.error(`Failed to execute content script: ${error.message}`);
+}
+
+/**
+ * When the popup loads, inject a content script into the active tab,
+ * and add a click handler.
+ * If we couldn't inject the script, handle the error.
+ */
+browser.tabs
+  .executeScript({ file: "/content_scripts/lender-search.js" })
+  .catch(reportExecuteScriptError);
